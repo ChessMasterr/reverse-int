30189499d02770f864161eff1cd3396400cdd8df
ChessMasterr <artiom.farhutdinoff@yandex.ru>
2023-01-16 16:08:49 +0500

    add decision

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    // x = n.toString().split("");.reverse().map(Number).join("");
+    n = n + "";
+    if (n.charAt() == "-") {
+        x = n.substring(1);
+        return x.split("").reverse().join("");
+    } else if (n.charAt(-1) == "0") {
+        x = n.slice(0, -1);
+        return x.split("").reverse().join("");
+    } else {
+        return n.split("").reverse().join("");
+    }
+};
